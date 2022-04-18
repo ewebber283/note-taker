de7bae3200a97696217d248f85e1199d1d674701
Eric Webber <ewebber283@gmail.com>
2022-04-18 17:55:07 -0400

    set up html routes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
+//const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 //parse incoming string or array data
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.use(express.json())
-app.use('/api', apiRoutes)
+//app.use('/api', apiRoutes)
 app.use('/', htmlRoutes)
 
 app.listen(PORT, () => {
