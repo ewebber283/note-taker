20c51923ea63c900df640593979926d6bff96c43
Eric Webber <ewebber283@gmail.com>
2022-04-18 18:27:22 -0400

    added api notes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,5 +1,24 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db.json');
-const { filterByQuery, findById, createNewNote, validateNote } = require('../../lib/notes');
+const { filterByQuery, createNewNote, validateNote } = require('../../lib/notes');
 
+router.get('/notes', (req, res) => {
+    let results = notes;
+
+    if(req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+})
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    if(!validateNote(req.body)) {
+        res.status(400)
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note)
+    }
+})
 module.exports = router;
